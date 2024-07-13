6790440465a0f83b46a7c22467d9dc8d72ab4401
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2024-07-13 22:36:20 +0100

    apply gofmt to main/mr*.go files

--- src/main/mrmaster.go
+++ src/main/mrmaster.go
@@ -9,10 +9,12 @@ package main
 // Please do not change this file.
 //
 
-import "../mr"
-import "time"
-import "os"
-import "fmt"
+import (
+	"../mr"
+	"fmt"
+	"os"
+	"time"
+)
 
 func main() {
 	if len(os.Args) < 2 {
